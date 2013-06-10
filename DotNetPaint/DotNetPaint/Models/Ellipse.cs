4bf9335cf934e8e50884322227f9464b6c2c13f3
Jakub Sikorski <jakubsikorski7@gmail.com>
2013-06-11 00:57:37 +0200

    Fix for drawing ellipse (missing fill)

--- DotNetPaint/DotNetPaint/Models/Ellipse.cs
+++ DotNetPaint/DotNetPaint/Models/Ellipse.cs
@@ -6,12 +6,14 @@ namespace DotNetPaint.Models
     public class Ellipse : IShape
     {
         public Pen Pen { get; set; }
+        public Brush Brush { get; set; }
         public Point Start { get; private set; }
         public Point End { get; set; }
 
-        public Ellipse(Pen pen, Point start, Point end)
+        public Ellipse(Pen pen, Brush brush, Point start, Point end)
         {
             Pen = pen;
+            Brush = brush;
             Start = start;
             End = end;
         }
@@ -20,6 +22,7 @@ namespace DotNetPaint.Models
         {
             var boundary = ShapeBoundary.GetFor(this);
             graphics.DrawEllipse(Pen, boundary);
+            graphics.FillEllipse(Brush, boundary);
         }
     }
 }
